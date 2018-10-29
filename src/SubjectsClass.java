ca3b3066a1781e19648301d5b364eb4d5d077b34
nbhatt22 <43207133+nbhatt22@users.noreply.github.com>
2018-10-29 08:05:23 -0400

    Add files via upload

---
+++ src/SubjectsClass.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author nandinibhatt
+ *
+ */
+public class SubjectsClass 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.print("My favorite subjects:\n\n\tComputerScience\n\n\tMath\n\n\tHistory");
+	}
+
+}
